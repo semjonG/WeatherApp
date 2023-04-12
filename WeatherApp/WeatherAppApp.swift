9c78b8d3f5f3427468fbbe7960f29a8ebf5345d8
Semjon Gerasimov <seme.gerasimov@x5.ru>
2023-04-12 14:08:22 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by mac on 12.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
